02d060e9afe99043baa8e25382408e010d1c7494
hrithikroboto <hrithik@robotostudio.com>
2025-05-22 11:38:35 +0530

    chore: add postinstall script to alert for data after installation

---
+++ apps/studio/scripts/cli-alert-for-data.ts
@@ -0,0 +1,31 @@
+async function main() {
+  console.log(
+    "\n\x1b[34m┌────────────────────────────────────────────────────────────────────────────┐\x1b[0m",
+  );
+  console.log(
+    "\x1b[34m│                                                                            │\x1b[0m",
+  );
+  console.log(
+    "\x1b[34m│  To seed sample data, run:                                                 │\x1b[0m",
+  );
+  console.log(
+    "\x1b[34m│                                                                            │\x1b[0m",
+  );
+  console.log(
+    "\x1b[34m│                                                                            │\x1b[0m",
+  );
+  console.log(
+    "\x1b[34m│                                                                            │\x1b[0m",
+  );
+  console.log(
+    "\x1b[34m│                                                                            │\x1b[0m",
+  );
+  console.log(
+    "\x1b[34m└────────────────────────────────────────────────────────────────────────────┘\x1b[0m\n",
+  );
+  console.log(
+    "\x1b[34m cd apps/studio && sanity exec scripts/create-data.ts --with-user-token    \x1b[0m",
+  );
+}
+
+main();
