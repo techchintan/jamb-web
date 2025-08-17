a069d20d828f960c4c330ac7ccda56f1d65ce4e0
voidhrithik <hrithik@robotostudio.com>
2025-08-18 00:32:33 +0530

    feat: add favicon, 404 page, robots.txt, sitemap, and Open Graph API endpoints
    - Introduced a favicon for the web application.
    - Created a custom 404 Not Found page to enhance user experience.
    - Added robots.txt for better SEO management.
    - Implemented a sitemap generation feature to improve site indexing.
    - Developed Open Graph API endpoints for dynamic SEO image generation and metadata handling.
    - Included draft mode functionality for Sanity integration.

---
+++ apps/web/src/app/robots.ts
@@ -0,0 +1,14 @@
+import { getBaseUrl } from "@/utils";
+import type { MetadataRoute } from "next";
+
+const baseUrl = getBaseUrl();
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
